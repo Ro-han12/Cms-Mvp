0a88860f30c09a70fad338c37423f17b16fa292d
rohan <rohannair2939@gmail.com>
2024-11-07 15:40:21 +0530

    trails

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Course Management Dashboard",
+  description: "Next.js Course Management System",
 };
 
 export default function RootLayout({
